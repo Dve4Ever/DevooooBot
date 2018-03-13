b55e699f4072212f7c71a47bf3ec694b763aab59
Dve4Ever <37259482+Dve4Ever@users.noreply.github.com>
2018-03-13 18:20:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,36 @@ client.on('message', message => {
     if (message.content === 'pee is stored in the balls') {
     	message.channel.send('Hey! Thats racist!');
     } 
+})
+
+client.on('message', message => {
+    if (message.content === 'Whats 9+10') {
+    	message.channel.send('21!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'your mum gay') {
+    	message.channel.send('Yours is too!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Retarted') {
+    	message.channel.send('Watch your Profanity!');
+    }
+});
+
+client.on('message', message => {
+    if (message.content === 'pee is stored in the balls') {
+    	message.channel.send('Hey! Thats racist!');
+    } 
+});
+
+client.on('message', message => {
+    if (message.content === 'weapon') {
+    	message.channel.send('Thats classified as an weapon!');
+    } 
 });
 
 // THIS  MUST  BE  THIS  WAY
