4d8ef23e7949402056a6257002d71e94b6a41095
Dve4Ever <37259482+Dve4Ever@users.noreply.github.com>
2018-03-14 11:58:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,6 +59,36 @@ client.on('message', message => {
     	message.channel.send('Thats classified as an weapon!');
     } 
 });
+ if (!suffix) {
+    var newamount = "2";
+  } else {
+    var amount = Number(suffix);
+    var adding = 1;
+    var newamount = amount + adding;
+  }
+  let messagecount = newamount.toString();
+  msg.channel
+    .fetchMessages({
+      limit: messagecount
+    })
+    .then(messages => {
+      msg.channel.bulkDelete(messages);
+      // Logging the number of messages deleted on both the channel and console.
+      msg.channel
+        .send(
+          "Deletion of messages successful. \n Total messages deleted including command: " +
+            newamount
+        )
+        .then(message => message.delete(5000));
+      console.log(
+        "Deletion of messages successful. \n Total messages deleted including command: " +
+          newamount
+      );
+    })
+    .catch(err => {
+      console.log("Error while doing Bulk Delete");
+      console.log(err);
+  });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
