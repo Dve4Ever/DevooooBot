e96cba5cf850ec3b68d117953ec862d4e93973a0
Dve4Ever <37259482+Dve4Ever@users.noreply.github.com>
2020-07-18 16:46:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,14 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
- 
-
 client.on('message', message => {
+    if (message.content === '!website') {
+       message.reply('My Website is: https://staydevoooo.com);}});
+client.on('message', message => {
+    if (message.content === 'test') {
+       message.reply('test');}});
 
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
 
 client.login(process.env.BOT_TOKEN);
